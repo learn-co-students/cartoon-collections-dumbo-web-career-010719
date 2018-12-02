ad080d48e99beea14ca8b57ac2418c0767b7bf6f
mazenswar <mazenswar@protonmail.com>
2018-12-02 16:46:36 -0500

    cartoon collections

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves arr   # code an argument here
   # Your code here
+  arr.each_with_index {|element, index| print "#{index + 1} #{element}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet arr     # code an argument here
   # Your code here
+  arr.collect {|elem| elem.capitalize! << "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls arr      # code an argument here
   # Your code here
+  arr.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese arr     # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find {|word| cheese_types.include?(word)}
 end
